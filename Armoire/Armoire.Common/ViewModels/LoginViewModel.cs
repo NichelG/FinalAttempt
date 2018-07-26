0465f7c80d655dbdbbdc53f85d85c60c8747c4dd
Sacha Gordon <sacha_gordon@hotmail.com>
2018-07-25 21:34:48 -0400

    Initial Login Setup

---
+++ Armoire/Armoire.Common/ViewModels/LoginViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Armoire.Common.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Please enter your username")]
+        public string UsernameRF { get; set; }
+
+        [Required(ErrorMessage = "Please enter your password")]
+        public string PasswordRF { get; set; }
+
+
+    }
+}
